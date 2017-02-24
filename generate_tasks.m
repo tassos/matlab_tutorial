e72cffcb709098f9ef72cc9530847cb05849e38d
Tassos (nakano) Natsakis <891234ser@eipieq.com>
2017-02-24 15:57:22 +0200

    Using a YAML file for generating the tasks database, this makes it so much easier to insert new questions

---
+++ generate_tasks.m
@@ -0,0 +1,20 @@
+function generate_tasks
+%GENERATE_TASKS Generate the tasks database
+%   It is much easier to define the tasks and all the required information
+%   in a YAML file, but a YAML file is much easier read by the students.
+%   That's why it can be useful to convert the YAML file into a matlab
+%   structure. If a student can browse through the matlab structure, then
+%   she probably aleady knows MATLAB and does not need this tutorial.
+%
+%   Prerequisites: YAMLMatlab library, that can be found here:
+%   https://github.com/beckel/nilm-eval/tree/master/Matlab/lib/YAMLMatlab_0.4.3
+
+if ~exist('tasks.yml','file')
+    tasks = ReadYaml(uiopen);
+else
+    tasks = ReadYaml('tasks.yml');
+end
+
+save('tasks.mat','tasks');
+end
+
